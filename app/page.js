38a7253cc73b71e5105ac6e4dba089e25e294c3b
Dulcinea Escuredo <dulcineaescuredo@gmail.com>
2026-06-16 18:53:52 -0300

    auto: update page.js"

--- app/page.js
+++ app/page.js
@@ -4301,6 +4301,8 @@ function DetalleAsunto({ asuntoActual, setAsuntoActual, setVista, clientes, hono
   const [nuevoLinkEtapa, setNuevoLinkEtapa] = useState({});
   const [uploading, setUploading] = useState(false);
   const [uploadingEtapa, setUploadingEtapa] = useState({});
+  const [filePreviewEtapa, setFilePreviewEtapa] = useState({});
+  const [hoverUploadEtapa, setHoverUploadEtapa] = useState({});
 
   useEffect(()=>{
     if (!a) return;
